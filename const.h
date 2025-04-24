de6ad3ab96c4089fb3ebcdea1c6fe4130fb2124f
XULIM <not3lbobillibo@gmail.com>
2025-04-24 00:09:11 -0400

    Add universal helper function, keywords, and constants

--- const.h
+++ const.h
@@ -1,11 +1,25 @@
 #ifndef CONST_H
+#define CONST_H
+
+#define private static
+
+#define PORT "6969"
 enum
 {
-    BUFLEN = 256,
+    BUFLEN = 255,
+    MAX_BUFLEN = 512,
     BACKLOG = 10,
     PFDS_INIT_LEN = 5,
     USERNAME_LEN = 32,
 };
 
-#define PORT "6969"
+static inline void *get_in_addr(struct sockaddr *s)
+{
+    if (s->sa_family == AF_INET)
+    {
+        return &(((struct sockaddr_in*)s)->sin_addr);
+    }
+    return &(((struct sockaddr_in6*)s)->sin6_addr);
+}
+
 #endif
