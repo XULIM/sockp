2da70bb19d1ce896f8c31dd8b48ab9833d86eb87
XULIM <not3lbobillibo@gmail.com>
2025-04-29 22:32:44 -0400

    Add char consts and term_str function

--- const.h
+++ const.h
@@ -1,9 +1,11 @@
 #ifndef CONST_H
 #define CONST_H
 
-#define private static
+#include <ctype.h>
 
+#define private static
 #define PORT "6969"
+
 enum
 {
     BUFLEN = 255,
@@ -11,15 +13,36 @@ enum
     BACKLOG = 10,
     PFDS_INIT_LEN = 5,
     USERNAME_LEN = 32,
+    CH_NEWLINE = '\n',
+    CH_NULL = '\0'
 };
 
-static inline void *get_in_addr(struct sockaddr *s)
+static inline void* get_in_addr(struct sockaddr *s)
 {
     if (s->sa_family == AF_INET)
     {
         return &(((struct sockaddr_in*)s)->sin_addr);
     }
     return &(((struct sockaddr_in6*)s)->sin6_addr);
+}
+
+/* Terminates str with termch.
+ * Paramters:
+ * - str: the string to be terminated.
+ * - strlen: the actual size of str.
+ * - maxlen: the max length of str.
+ * - termch: the terminal character.
+ **/
+static inline void term_str(char *str, int strlen, int maxlen, char termch)
+{
+    if (iscntrl(str[strlen-1]) || strlen == maxlen)
+    {
+        str[strlen-1] = termch;
+    }
+    else
+    {
+        str[strlen] = termch;
+    }
 }
 
 #endif
